c0fe2c24b89c03bb32ddd2328ee2d711fc2be686
Katherine Mazariegos <katherinemazariegos@ufm.edu>
2019-11-05 16:01:36 -0600

    Async tutorial done

---
+++ async-whilst.js
@@ -0,0 +1,34 @@
+var async = require('async'),
+    http = require('http');
+
+var lastResponse = '',
+    totalRequests = 0,
+    url = process.argv[2];
+
+console.log("3")
+
+async.whilst(function () { return lastResponse !== 'meerkat'; },
+  function (callback) {
+    http.get(url, function (res) {
+      totalRequests++;
+      
+      var body = ''; 
+      res.on('data', function (chunk) {
+        body += chunk.toString();
+      });
+
+      res.on('end', function () {
+        lastResponse = body.trim();
+        callback();
+      });
+
+    }).on('error', function (err) {
+      return console.error(err);
+    });
+  },
+
+  function (err) {
+    console.log(totalRequests);
+    console.log("3")
+  }
+);
---
+++ waterfall.js
@@ -0,0 +1,30 @@
+var http = require('http'),
+async = require('async'),
+fs = require('fs');
+
+async.waterfall([
+    function (callback) {
+        fs.readFile(process.argv[2], function doneReading(err, fileContents) {
+            var body = fileContents.toString();
+            callback(null, body);
+        })
+    },
+
+    function (body, callback) {
+        var url = body;
+        var body = '';
+        http.get(url, function (res) {
+            res.on('data', function (chunk) {
+                body += chunk.toString();
+            });
+            res.on('end', function () {
+                callback(null, body);
+            });
+        }).on('error', function (err) {
+            callback(err);
+        });
+    }
+], function (err, result) {
+    if (err) return console.error(result);
+    console.log(result);
+});
